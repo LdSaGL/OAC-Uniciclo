85812e700d16f23fbcfc544c7b5b279bcff6af39
LdSaGL <lucas.sa.licio@gmail.com>
2024-12-31 20:13:39 -0300

    Finalizado o immGen e adicao do git ignore

--- immGen.vhd
+++ immGen.vhd
@@ -14,8 +14,27 @@ architecture immGenArch of immGen is
         begin
             -- cada opcode tem 7 bits
             case instruction (6 downto 0) is
-            when "0110011" =>  -- R-type instructions
+            when "0110011" =>  -- R-type
             imm <= resize(X"0", 32);
+
+            when "0000011" | "0010011" | "1100111" =>  -- I-type
+            imm <= resize(SIGNED((instruction(31 downto 20))), 32);
+
+            when "0100011" =>  -- S-type
+            imm <= resize(SIGNED((instruction(31 downto 25) & instruction(11 downto 7))), 32);
+
+            when "1100011" =>  -- SB-type
+            imm <= resize(SIGNED((instruction(31) & instruction(7) & instruction(30 downto 25) & instruction(11 downto 8) & "0")), 32);
+
+            when "1101111" =>  -- UJ-type
+            imm <= resize(SIGNED((instruction(31) & instruction(19 downto 12) & instruction(20) & instruction(30 downto 21) & "0")), 32);
+
+            when "0110111" => -- U-type
+            imm <= resize(SIGNED((instruction(31 downto 12) & "000000000000")), 32);
+
+            when others => -- zerar em todos os casos nao mapeados
+            imm <= resize(X"0", 32);
+            
         end case;
     end process;
 end immGenArch;
