d482fc951ca350dbf4462dedd7012cd44bb9df10
LdSaGL <lucas.sa.licio@gmail.com>
2025-01-31 22:44:16 -0300

    Implementação do banco de registradores

---
+++ xregs.vhd
@@ -0,0 +1,41 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity XREGS is
+    generic (WSIZE : natural := 32);
+    port (
+        clk, wren: in std_logic; 
+        rs1, rs2, rd: in std_logic_vector(4 downto 0);
+        data: in std_logic_vector(WSIZE-1 downto 0); 
+        ro1, ro2: out std_logic_vector(WSIZE-1 downto 0));
+end XREGS;
+
+architecture xreg_arch of XREGS is
+    type reg_array is array (0 to 31) of std_logic_vector(WSIZE-1 downto 0);
+    signal regs: reg_array;
+
+begin
+    process(clk)
+        begin
+            if rising_edge(clk) then
+                if (to_integer(unsigned(rs1)) = 0) then
+                    ro1 <= (others => '0');
+                elsif (to_integer(unsigned(rs1)) /= 0) then
+                    ro1 <= regs(to_integer(unsigned(rs1)));
+                end if;
+                if (to_integer(unsigned(rs2)) = 0) then
+                    ro2 <= (others => '0');
+                elsif (to_integer(unsigned(rs2)) /= 0) then
+                    ro2 <= regs(to_integer(unsigned(rs2)));
+                end if;
+                if wren = '1' then
+                    if (to_integer(unsigned(rd)) = 0) then
+                        regs(to_integer(unsigned(rd))) <= (others => '0');
+                    else
+                        regs(to_integer(unsigned(rd))) <= data;
+                    end if; 
+                end if;
+            end if;
+	end process;
+end xreg_arch ;
---
+++ xregs_tb.vhd
@@ -0,0 +1,97 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity xregs_tb is
+end entity xregs_tb;
+
+architecture xregs_tb_arch of xregs_tb is
+
+    component XREGS is
+        generic (WSIZE : natural := 32);
+        port (
+            clk, wren: in std_logic; 
+            rs1, rs2, rd: in std_logic_vector(4 downto 0);
+            data: in std_logic_vector(WSIZE-1 downto 0); 
+            ro1, ro2: out std_logic_vector(WSIZE-1 downto 0));
+    end component;
+
+    signal clk, wren: std_logic := '0';
+    signal rs1, rs2, rd: std_logic_vector(4 downto 0) := (others => '0');
+    signal data: std_logic_vector(31 downto 0);
+    signal ro1, ro2: std_logic_vector(31 downto 0);
+
+begin
+
+    Dut: XREGS port map (
+            clk => clk, wren => wren, 
+            rs1 => rs1, rs2 => rs2, rd => rd,
+            data => data, 
+            ro1 => ro1, ro2 => ro2);
+    
+    process
+        begin
+        data <= X"24924927";
+        rd <= "00011";
+        wren <= '1';
+        clk <= '1';
+        wait for 10 ns;
+        clk <= '0';
+        wait for 10 ns;
+
+        data <= X"76b2cd77";
+        rd <= "00100";
+        wren <= '1';
+        clk <= '1';
+        wait for 10 ns;
+        clk <= '0';
+        wait for 10 ns;
+
+        rs1 <= "00011";
+        rs2 <= "00100";
+        wren <= '0';
+        clk <= '1';
+        wait for 10 ns;
+        clk <= '0';
+        wait for 10 ns;
+
+        assert (ro1 = X"24924927") report "IN READ/WRITE: Wrong register 1 content" severity error;
+        assert (ro2 = X"76b2cd77") report "IN READ/WRITE: Wrong register 2 content" severity error;
+
+        data <= X"24924927";
+        rd <= "00000";
+        wren <= '1';
+        clk <= '1';
+        wait for 10 ns;
+        clk <= '0';
+        wait for 10 ns;
+
+        rs1 <= "00000";
+        wren <= '0';
+        clk <= '1';
+        wait for 10 ns;
+        clk <= '0';
+        wait for 10 ns;
+
+        assert (ro1 = X"00000000") report "Content of register 0 modified" severity error;
+
+        clk <= '1';
+        wait for 10 ns;
+        clk <= '0';
+        wait for 10 ns;
+
+        rs1 <= "00011";
+        rs2 <= "00100";
+        wren <= '0';
+        clk <= '1';
+        wait for 10 ns;
+        clk <= '0';
+        wait for 10 ns;
+
+        assert (ro1 = X"00000000") report "IN RESET: Wrong register 1 content" severity error;
+        assert (ro2 = X"00000000") report "IN RESET: Wrong register 2 content" severity error;
+
+        assert false report "Test done." severity note;
+        wait;
+    end process;
+    end architecture xregs_tb_arch;
